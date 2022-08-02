eda73d83c2a9a211a0d8c7c4783669890cd36f24
heywc <1842347744@qq.com>
2022-08-02 21:22:16 +0800

    增加获取参数/删除参数

--- demo/2022-08/2022-08-02/index.js
+++ demo/2022-08/2022-08-02/index.js
@@ -2,7 +2,7 @@
  * @Author: heywc “1842347744@qq.com”
  * @Date: 2022-08-02 11:57:06
  * @LastEditors: heywc “1842347744@qq.com”
- * @LastEditTime: 2022-08-02 13:01:29
+ * @LastEditTime: 2022-08-02 21:09:35
  * @FilePath: /DailyLearning/demo/2022-08/2022-08-02/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -52,4 +52,45 @@ function urlencode(clearString) {
     return output;
 }
 
+
+function getUrlParams(field) {
+    // 通过 ? 分割获取后面的参数字符串
+    if(!window.location.href.includes('?')) {
+        return ''
+    }
+    let urlStr = window.location.href.split('?')[1];
+    // 创建空对象存储参数
+    let obj = {};
+    // 再通过 & 将每一个参数单独分割出来
+    let paramsArr = urlStr.split('&');
+    for(let i = 0,len = paramsArr.length;i < len;i++){
+        // 再通过 = 将每一个参数分割为 key:value 的形式
+        let arr = paramsArr[i].split('=');
+        obj[arr[0]] = arr[1];
+    }
+    return obj[field];
+}
+
+function funcUrlDel(name){
+    var loca = window.location;
+    var baseUrl = loca.origin + loca.pathname + "?";
+    var query = loca.search.substr(1);
+    if (query.indexOf(name)>-1) {
+        var obj = {}
+        var arr = query.split("&");
+        for (var i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].split("=");
+            obj[arr[i][0]] = arr[i][1];
+        };
+        delete obj[name];
+        var url = baseUrl + JSON.stringify(obj).replace(/[\"\{\}]/g,"").replace(/\:/g,"=").replace(/\,/g,"&");
+        return url
+    }else{
+        return window.location.href;
+    };
+}
+console.log(funcUrlDel('id'));
+
 console.log(urlencode('https://app37766.eapps.dingtalkcloud.com/web/index.html?corpId=ding159e37c963eae133&state=taskDetails,7481f1df-be0a-4b01-88fe-207acf7efd89&ddtab=true'));
+
+console.log(getUrlParams('id'));
